ef41e5158a6bbf0d2c79c974b9e955a41cdf1e57
Sebastian Mischke <SebastianMischke@gmx.de>
2017-06-29 17:17:25 +0200

    Small comment change

--- SiteHandler.py
+++ SiteHandler.py
@@ -45,8 +45,8 @@ def curse(addonpage):
     try:
         page = requests.get(addonpage + '/download')
         contentString = str(page.content)
-        indexOfZiploc = contentString.find('data-href') + 11  # Will be the index of the first char of the url
-        endQuote = contentString.find('"', indexOfZiploc)  # Will be the index of the ending quote after the url
+        indexOfZiploc = contentString.find('data-href') + 11  # first char of the url
+        endQuote = contentString.find('"', indexOfZiploc)  # ending quote after the url
         return contentString[indexOfZiploc:endQuote]
     except Exception:
         print('Failed to find downloadable zip file for addon. Skipping...\n')
